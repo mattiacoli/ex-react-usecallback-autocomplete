ca23088155ea8b6bcfdefb887abc3a2dc83f8383
mattiacoli <mattia.coli@aol.com>
2025-06-25 12:07:37 +0200

    Milestone 1 , add  searchbar and dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [query, setQuery] = useState('')
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3333/products?search=${query}`)
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [query])
+
+  console.log(products);
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mt-4">
+
+        <input
+          type="text"
+          className='form-control'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder='Cerca ...'
+        />
+
+
+        {query &&
+
+          <div className="dropdown-menu show w-100 ">
+            {products.map(product => (
+              <a
+                key={product.id}
+                className="dropdown-item"
+                href="#"
+              >
+                {product.name}
+              </a>
+            ))}
+          </div>
+
+
+        }
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
